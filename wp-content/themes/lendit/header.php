beb41568542dcd2758c1a415904d739eb43f17c6
Katherine Santiago <santiago.kat@gmail.com>
2014-09-02 17:04:00 -0400

    added custom nav

--- wp-content/themes/lendit/header.php
+++ wp-content/themes/lendit/header.php
@@ -1,1 +1,11 @@
 <p>This is the header.</p>
+
+<?php
+
+  $args = array(
+    'menu' => 'Main Menu'
+  );
+
+  wp_nav_menu( $args );
+
+?>
--- wp-content/themes/lendit/home.php
+++ wp-content/themes/lendit/home.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
-<p>This is the front page</p>
+<p>This is the home.php file.</p>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+  <h3><?php the_title(); ?></h3>
+  <?php the_content(); ?>
+  <hr>
+
+<?php endwhile; else: ?>
+
+  <p>There are no posts here.</p>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
