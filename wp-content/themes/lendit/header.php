ae8c0a37347eb9bf720e09c362abbb1a868ef3df
nphillips <nick@nphillips.com>
2014-11-05 18:25:40 -0500

    Consistent nav between blog and site

--- wp-content/themes/lendit/header.php
+++ wp-content/themes/lendit/header.php
@@ -1,74 +1,75 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 
 
-  <title>
-    <?php
-    wp_title( '-', true, 'right' );
+	<title>
+<?php
+wp_title( '-', true, 'right' );
 
-    bloginfo( 'name' );
-    ?>
-  </title>
-  <link rel="stylesheet" type="text/css" href="//cloud.typography.com/7219312/623924/css/fonts.css" />
-  <?php wp_head(); ?>
-  <meta name="google-site-verification" content="by6W7B4QIEZf5yc1MoQlex0GuqL2U9ekI9YTCSzX5wg" />
+bloginfo( 'name' );
+?>
+	</title>
+	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7219312/623924/css/fonts.css" />
+	<?php wp_head(); ?>
+	<meta name="google-site-verification" content="by6W7B4QIEZf5yc1MoQlex0GuqL2U9ekI9YTCSzX5wg" />
 
-  <link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico">
+	<link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico">
 </head>
 <body>
 
-  <!-- Google Analytics Code -->
-  <script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-12735343-10']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_setDomainName', 'lendit.co']);
-  _gaq.push(['_trackPageview']);
+	<!-- Google Analytics Code -->
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-12735343-10']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_setDomainName', 'lendit.co']);
+_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-  </script>
-  <!-- End of Google Analytics Code -->
-
-  <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
-    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-     <span class="sr-only">Toggle navigation</span>
-     <span class="icon-bar"></span>
-     <span class="icon-bar"></span>
-     <span class="icon-bar"></span>
-   </button>
-   <div class="container">
-     <div id="navbarCollapse" class="collapse navbar-collapse">
-
-      <div class="nav-logo">
-        <a href="/">
-         <img src="https://s3-us-west-2.amazonaws.com/lendit/lenditlogo_toscale_header_nodrpshadow.png" alt="LendIt" title="LendIt">
-       </a>
-     </div>
-
-     <!-- This is the Nav Menu -->
-     <?php
-       wp_nav_menu( array(
-        'menu'              => 'Main Menu',
-        'theme_location'    => 'lendit',
-        'depth'             => 2,
-        'container'         => 'div',
-        'container_class'   => 'collapse navbar-collapse',
-        'container_id'      => 'bs-example-navbar-collapse-1',
-        'menu_class'        => 'nav navbar-nav',
-        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-        'walker'            => new wp_bootstrap_navwalker())
-       );
-     ?>
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+	<!-- End of Google Analytics Code -->
 
 
-   </div>
- </div>
-</nav>
+<div class="navbar-area">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<div class="logo-area">
+				<a href="/" class="nav-logo"></a>
+			</div>
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+
+		 <!-- This is the Nav Menu -->
+<?php
+wp_nav_menu( array(
+	'menu'              => 'Main Menu',
+	'theme_location'    => 'lendit',
+	'depth'             => 2,
+	'container'         => 'div',
+	'container_class'   => 'collapse navbar-collapse',
+	'container_id'      => 'bs-example-navbar-collapse-1',
+	'menu_class'        => 'nav navbar-nav',
+	'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+	'walker'            => new wp_bootstrap_navwalker())
+);
+?>
+
+				</div>
+		</div>
+	</nav>
+</div>
+
+	
 
